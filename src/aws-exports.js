22e3a137337f1630fa35f8aced79479f80b9a6c0
dnikitin437 <yrtimdnitikin@gmail.com>
2021-02-25 01:38:11 +0300

    добавил api

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "us-east-2",
     "aws_user_pools_id": "us-east-2_qd1ZvQdml",
     "aws_user_pools_web_client_id": "4ivnp2ogv9rd7d5jg4q59fd5ir",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://khvia54wkbg2te443peadebuhq.appsync-api.us-east-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-ebhq4u6y35gfzkk7argbs2m26m"
 };
 
 
